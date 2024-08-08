171c634506f4412969f71257fe14ef824c9a0f28
Gia Phuc <giaphuc29082004@gmail.com>
2024-08-08 14:57:22 +0700

    fixed the functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import schedule
 import time
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -18,7 +19,7 @@ def send_discord_message(msg: str, channel_id: str):
 
 # send_discord_message('hello world', '1129419871046865031')
 
-schedule.every().second.do(send_discord_message, 'hello world', '1129419871046865031')
+schedule.every().day.at("6:00").do(send_discord_message, 'Good morning', '1129419871046865031')
 
 while True:
     schedule.run_pending()
