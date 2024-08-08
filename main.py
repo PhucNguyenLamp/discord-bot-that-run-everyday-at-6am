2b87e69d08819f2db76b6f2c651af39b014e386c
Gia Phuc <giaphuc29082004@gmail.com>
2024-08-08 07:46:52 +0700

    no secret now

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import schedule
+import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+def send_discord_message(msg: str, channel_id: str):
+    headers = {
+        'Authorization': f'Bot {TOKEN}',
+        'Content-Type': 'application/json',
+    }
+    message = json.dumps({'content': msg})
+
+    req = requests.post(f'https://discordapp.com/api/channels/{channel_id}/messages', headers=headers, data=message)
+
+# send_discord_message('hello world', '1129419871046865031')
+
+schedule.every().second.do(send_discord_message, 'hello world', '1129419871046865031')
+
+while True:
+    schedule.run_pending()
+    time.sleep(5)
